d6a7ca8d310beb949cd9fba5e2387609c1843801
fabian sguiglia <fsguiglia@gmail.com>
2024-05-08 12:12:57 -0300

    key/value

--- app/components/Carousel/index.tsx
+++ app/components/Carousel/index.tsx
@@ -4,7 +4,11 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Image from "next/image";
 
 const Slider = () => {
-const slides = ["/1.png", "/2.png", "/3.png", "/4.png", "/5.png"]
+const slides = [{id: '1', value: '/1.png'},
+                {id: '2', value: '/2.png'},
+                {id: '3', value: '/3.png'},
+                {id: '4', value: '/4.png'},
+                {id: '5', value: '/5.png'}];
   return (
     <div>
       <Carousel
@@ -18,7 +22,7 @@ const slides = ["/1.png", "/2.png", "/3.png", "/4.png", "/5.png"]
         {slides.map((item) => (
           <div>
             <div>
-                <Image src={item} width="1366" height="738" alt="MNT" />
+                <Image src={item.value} width="1366" height="738" alt="MNT" />
             </div>
           </div>
         ))}

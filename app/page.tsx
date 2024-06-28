d171eb3660489d530163a8bb4d1b4c53e689c7f9
fabian sguiglia <fsguiglia@gmail.com>
2024-06-28 12:45:54 -0300

    dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="pl-12 pt-20">
       <div className="md:hidden pr-8 mb-10">
         <h1 className="mb-4 2xl:mb-8 font-extrabold leading-none tracking-tight text-gray-900 text-4xl md:text-5xl lg:text-6xl 2xl:text-8xl text-center">
-          El mapa no es el territorio
+          El mapa no es el territorios
         </h1>
       </div>
 
@@ -19,7 +19,7 @@ export default async function Home() {
         </div>
         <div className="md:pr-5 content-stretch">
           <div>
-            <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl 2xl:text-8xl dark:text-white pb-4 hidden md:block">
+            <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl 2xl:text-8xl pb-4 hidden md:block">
               El mapa no es el territorio
             </h1>
           </div>
@@ -49,36 +49,36 @@ export default async function Home() {
 
       <div className="md:min-h-[calc(100vh-100px)] md:mr-8 mr-12 scroll-mt-40 mb-20 md:mb-0" id="descargas">
       <div>
-            <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
               Descargas
             </h1>
           </div>
           <div className="md:text-2xl 2xl:text-4xl">
             <p className="pb-5">
-              <Link href="https://d17ccq4m3xjsbi.cloudfront.net/mnt/MNT2.zip" className="bg-sky-100 dark:bg-white dark:text-black">
+              <Link href="https://d17ccq4m3xjsbi.cloudfront.net/mnt/MNT2.zip" className="bg-sky-100">
                 Ejecutable (Windows x64)
               </Link>. 
-              Requiere <Link href="https://aka.ms/vs/16/release/vc_redist.x64.exe" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">Microsoft Visual C++ Redistributable for VS 2017</Link>
+              Requiere <Link href="https://aka.ms/vs/16/release/vc_redist.x64.exe" target="_blank" className="bg-sky-100">Microsoft Visual C++ Redistributable for VS 2017</Link>
             </p>
             <p className="pb-5">
-              <Link href="http://www.sguiglia.com.ar/mnt/descargas/MNT_guia.pdf" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">Guía de Inicio Rápido</Link>
+              <Link href="http://www.sguiglia.com.ar/mnt/descargas/MNT_guia.pdf" target="_blank" className="bg-sky-100">Guía de Inicio Rápido</Link>
             </p>
             <p className="pb-5">
-              <Link href="https://github.com/fsguiglia/Concatenate" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">Concatenate para Max for Live</Link>
+              <Link href="https://github.com/fsguiglia/Concatenate" target="_blank" className="bg-sky-100">Concatenate para Max for Live</Link>
             </p>
             <p className="pb-5">
-              <Link href="https://github.com/fsguiglia/MNT2" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">Código fuente</Link>
+              <Link href="https://github.com/fsguiglia/MNT2" target="_blank" className="bg-sky-100">Código fuente</Link>
             </p>
           </div>
       </div>
 
       <div className="grid grid-cols-1 md:mr-8 mr-12 md:grid-cols-2 md:min-h-[calc(100vh-100px)] content-start scroll-mt-40 mb-20 md:mb-0" id="tutoriales">
         <div>
-          <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white pb-4">
+          <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl pb-4">
             Tutoriales
           </h1>
           <p className="pb-5 md:text-2xl 2xl:text-4xl">
-            <Link href="https://youtube.com/playlist?list=PLEMVlXTcC5rn0nzcSsK7CfwKhJ1gFrZIB&si=QrlbriWbq-XBzJvM" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">Ver en YouTube</Link>
+            <Link href="https://youtube.com/playlist?list=PLEMVlXTcC5rn0nzcSsK7CfwKhJ1gFrZIB&si=QrlbriWbq-XBzJvM" target="_blank" className="bg-sky-100">Ver en YouTube</Link>
           </p>
         </div>
         <div className="w-full aspect-video md:pl-5 pb-10 hidden md:block">
@@ -88,7 +88,7 @@ export default async function Home() {
       
       <div className="md:min-h-[calc(100vh-100px)] md:mr-8 mr-12 scroll-mt-40 mb-20 md:mb-0" id="publicaciones">
         <div>
-          <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white pb-4">
+          <h1 className="mb-4 2xl:mb-8 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl pb-4">
             Publicaciones
           </h1>
         </div>
@@ -97,7 +97,7 @@ export default async function Home() {
               Sguiglia, F. (2024).
               MNT2. Una caja de herramientas para el diseño de instrumentos y entornos interactivos.
               En&nbsp;
-              <Link href="https://en.cmmas.com/sonicideas" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">
+              <Link href="https://en.cmmas.com/sonicideas" target="_blank" className="bg-sky-100">
                 Ideas Sónicas
               </Link>
               &nbsp;(en prensa).
@@ -110,15 +110,15 @@ export default async function Home() {
           <p className="pb-5 2xl:pb-10">
               Sguiglia, F. (2023).
               Síntesis concatenativa basada en corpus.
-              En <Link href="https://wolkowiczeditores.mitiendanube.com/productos/investigaciones-sobre-audio-espacial-y-estetica-del-arte-sonoro-libro-digital-incluye-archivos-de-audio/" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">Investigaciones sobre audio espacial y estética del arte sonoro</Link>. 
+              En <Link href="https://wolkowiczeditores.mitiendanube.com/productos/investigaciones-sobre-audio-espacial-y-estetica-del-arte-sonoro-libro-digital-incluye-archivos-de-audio/" target="_blank" className="bg-sky-100">Investigaciones sobre audio espacial y estética del arte sonoro</Link>. 
           </p>
           <p>
             Sguiglia, F., Coton, P. y Toth, F. (2019).
-            <Link href="https://www.nime.org/proceedings/2019/nime2019_paper029.pdf" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">
+            <Link href="https://www.nime.org/proceedings/2019/nime2019_paper029.pdf" target="_blank" className="bg-sky-100">
               El Mapa no es el Territorio: sensor mapping for audiovisual performances
             </Link>
             . En&nbsp;
-            <Link href="https://www.nime.org/archives/" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">
+            <Link href="https://www.nime.org/archives/" target="_blank" className="bg-sky-100">
             Proceedings of the international conference on New Interfaces for Musical Expression
             </Link>
             &nbsp;({`NIME '19`}).
@@ -128,7 +128,7 @@ export default async function Home() {
 
       <div className="md:min-h-[calc(100vh-140px)] md:mr-8 mr-12 scroll-mt-40 mb-20 md:mb-0" id="contacto">
         <div>
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white pb-4">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl pb-4">
             Contacto
           </h1>
         </div>
@@ -137,7 +137,7 @@ export default async function Home() {
             MNT es un proyecto de Fabián Sguiglia.
           </p>
           <p>
-            <Link href="http://www.sguiglia.com.ar" target="_blank" className="bg-sky-100 dark:bg-white dark:text-black">www.sguiglia.com.ar</Link>
+            <Link href="http://www.sguiglia.com.ar" target="_blank" className="bg-sky-100">www.sguiglia.com.ar</Link>
           </p>
         </div>
       </div>

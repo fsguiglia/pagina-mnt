f8d996290e083ffb2fb9740a92232833d145f0f8
fabian sguiglia <fsguiglia@gmail.com>
2024-07-04 09:18:11 -0300

    test next img

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Page () {
+    return(
+        <div>
+            <Image src="https://media.mnt.com.ar/image/720/1.png" width="1366" height="738" alt="MNT" />
+        </div>
+    )
+}
